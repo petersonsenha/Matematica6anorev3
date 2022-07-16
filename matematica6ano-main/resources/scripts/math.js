17da18a830caffc08477c5038467f16e8e4f3a52
Peterson Senha <peterson.senha@gmail.com>
2022-07-16 15:48:04 -0300

    Mudança de Repositório
    Mudança de repositório

---
+++ matematica6ano-main/resources/scripts/math.js
@@ -0,0 +1,12 @@
+<script
+      type="text/javascript"
+      src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+</script>
+<script type="text/x-mathjax-config">
+    MathJax.Hub.Config({
+        tex2jax: { inlineMath: [["$","$"],["\\(","\\)"]] },
+        "HTML-CSS": {
+        linebreaks: { automatic: true, width: "container" }
+        }
+    });
+</script>
